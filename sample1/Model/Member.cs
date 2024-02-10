25c484563ef5cfadee567d6c334776d6e4a911e0
sodajin <mikana.fuyu@gmail.com>
2024-02-10 20:53:28 +0800

    member

---
+++ sample1/Model/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Model
+{
+    public class Member
+    {
+        public int MemberID { get; set; }
+        public string MemberFirstName{ get; set; }
+        public string MemberMiddleName { get; set; }
+        public string MemberLastName { get; set; }
+    }
+}
