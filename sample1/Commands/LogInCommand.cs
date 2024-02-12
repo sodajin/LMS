3aca8de01ad8d215f92a9e24e1f7806296aec74f
lezzthanthree <arisatominato48@gmail.com>
2024-02-12 09:13:13 +0800

    Show message if Invalid Credentials

--- sample1/Commands/LogInCommand.cs
+++ sample1/Commands/LogInCommand.cs
@@ -1,29 +1,44 @@
 ﻿using LibraryManagementSystem.Store;
 using LibraryManagementSystem.ViewModel;
 using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace LibraryManagementSystem.Commands
 {
     public class LogInCommand : CommandBase
     {
-        private LogInViewModel _viewModel;
-        private NavigationStore _navigationStore;
-        private Func<ViewModelBase> _createAdminDashboardViewModel;
-
+        private readonly LogInViewModel _viewModel;
+        private readonly NavigationStore _navigationStore;
+        private readonly Func<ViewModelBase> _createAdminDashboardViewModel;
+        private readonly Action<object> _changeAuthMessage;
+        private readonly Predicate<object> _canExecute; 
 
         public LogInCommand(
             LogInViewModel viewModel,
             NavigationStore navigationStore, 
-            Func<ViewModelBase> createAdminDashboardViewModel
+            Func<ViewModelBase> createAdminDashboardViewModel,
+            Action<object> changeAuthMessage,
+            Predicate<object> canExecute = null
         ) {
             _viewModel = viewModel;
             _navigationStore = navigationStore;
             _createAdminDashboardViewModel = createAdminDashboardViewModel;
+            _changeAuthMessage = changeAuthMessage ?? throw new ArgumentNullException(nameof(changeAuthMessage));
+
         }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
 
         public override void Execute(object parameter)
         {
@@ -37,7 +52,7 @@ namespace LibraryManagementSystem.Commands
                 return;
             }
 
-            _viewModel.AuthMessage = "INVALID CREDENTIALS";
+            _changeAuthMessage(parameter);
             _viewModel.ShakeAuth = true;
             _viewModel.ShakeAuth = false;
         }
