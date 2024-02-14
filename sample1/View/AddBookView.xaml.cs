7274e2bb313440ab35f916772a66421fc3282834
sodajin <mikana.fuyu@gmail.com>
2024-02-14 14:10:20 +0800

    push desing

--- sample1/View/AddBookView.xaml.cs
+++ sample1/View/AddBookView.xaml.cs
@@ -24,5 +24,10 @@ namespace sample1.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
