d4f6228d72afb7e94d9569327e014eb39ef5a992
lezzthanthree <arisatominato48@gmail.com>
2024-02-16 08:10:57 +0800

    Don't allow empty strings

--- sample1/Commands/AddMemberCommand.cs
+++ sample1/Commands/AddMemberCommand.cs
@@ -29,8 +29,13 @@ namespace LibraryManagementSystem.Commands
         }
         public override void Execute(object parameter)
         {
+            if (CheckData() == true)
+            {
+
+            }
+
             User newUser = new User(
-                _viewModel.ID,    
+                _viewModel.ID,
                 _viewModel.FirstName,
                 _viewModel.MiddleName,
                 _viewModel.LastName,
@@ -39,8 +44,21 @@ namespace LibraryManagementSystem.Commands
                 100, AccountType.Simple
             );
 
-            _userList.SignUp( newUser );
+            _userList.SignUp(newUser);
             _dashboardNavigationStore.CurrentViewModel = _createManageMemberViewModel(new List<User>(), "");
+        }
+
+        public bool CheckData()
+        {
+            if (
+                _viewModel.ID == null || _viewModel.ID == "" ||
+                _viewModel.FirstName == null || _viewModel.FirstName == "" ||
+                _viewModel.MiddleName == null || _viewModel.MiddleName == "" ||
+                _viewModel.LastName == null || _viewModel.LastName == "" ||
+                _viewModel.Username == null || _viewModel.Username == "" ||
+                _viewModel.Password == null || _viewModel.Password == ""
+                ) return false;
+                return true;
         }
     }
 }
